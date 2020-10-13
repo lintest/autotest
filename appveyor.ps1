4bdea5c2c00fd2b0c8686f0279a7be1cc039e5d9
Kandrashin Denis <mail@lintest.ru>
2020-10-14 00:17:47 +0300

    CREATEINFOBASE

--- appveyor.ps1
+++ appveyor.ps1
@@ -10,3 +10,14 @@ if (!(Test-Path $dist1c)) {
 Write-Host "Expand archive 1cv8.zip ..."
 Expand-Archive -Force -Path $dist1c -DestinationPath $path
 Set-Content "$path\bin\conf\conf.cfg" "DisableUnsafeActionProtection=.*;"
+
+$config = "$path\config"
+$database = "$path\test"
+Write-Host "Create infobase"
+New-Item -ItemType Directory -Force -Path "$database" | Out-Null
+Start-Process "bin\1cv8t.exe" -ArgumentList "CREATEINFOBASE File=$database" -Wait
+Write-Host "Load config..."
+Start-Process "bin\1cv8t.exe" -ArgumentList "DESIGNER /F $database /LoadConfigFromFiles $config /UpdateDBCfg"  -Wait
+Write-Host "Process autotest..."
+Start-Process "bin\1cv8ct.exe" -ArgumentList "ENTERPRISE /F $database" -Wait
+Write-Host "Autotest finished"
