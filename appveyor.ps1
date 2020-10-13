2694c0a834855092e928b280e319cc0057d991b4
Kandrashin Denis <mail@lintest.ru>
2020-10-14 00:14:27 +0300

    create from command line

--- appveyor.ps1
+++ appveyor.ps1
@@ -10,12 +10,3 @@ if (!(Test-Path $dist1c)) {
 Write-Host "Expand archive 1cv8.zip ..."
 Expand-Archive -Force -Path $dist1c -DestinationPath $path
 Set-Content "$path\bin\conf\conf.cfg" "DisableUnsafeActionProtection=.*;"
-
-Write-Host "Create infobase"
-New-Item -ItemType Directory -Force -Path "$path\test" | Out-Null
-Start-Process "bin\1cv8ct.exe" -ArgumentList "CREATEINFOBASE File=$path\test" -Wait
-Write-Host "Load config..."
-Start-Process "bin\1cv8t.exe" -ArgumentList "DESIGNER /F $path\test /LoadConfigFromFiles $path\config /UpdateDBCfg"  -Wait
-Write-Host "Process autotest..."
-Start-Process "bin\1cv8ct.exe" -ArgumentList "ENTERPRISE /F $path\Test" -Wait
-Write-Host "Autotest finished"
