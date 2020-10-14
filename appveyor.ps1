527e10284e44cff6ff9e22c26cb1fcf37dd1afae
Kandrashin Denis <mail@lintest.ru>
2020-10-14 20:41:00 +0300

    Download file

--- appveyor.ps1
+++ appveyor.ps1
@@ -1,9 +1,7 @@
+$dist1c = "$path\1cv8t.7z"
 $url1c = $env:URL_1CV8T
 $path = $env:APPVEYOR_BUILD_FOLDER
-
-$dist1c = "$path\1cv8t.7z"
 if (!(Test-Path $dist1c)) {
   Write-Host "Download 1cv8t.7z ..."
   Invoke-WebRequest -Uri $url1c -OutFile $dist1c
 }
-Write-Host "Expand archive 1cv8t.7z ..."
