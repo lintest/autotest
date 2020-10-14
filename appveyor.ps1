0cecdbce0fa7e7e612d620b9fa2069fec109ef20
Kandrashin Denis <mail@lintest.ru>
2020-10-14 19:23:56 +0300

    Сборка перенесена в файл appveyor.yml

--- appveyor.ps1
+++ appveyor.ps1
@@ -10,15 +10,3 @@ if (!(Test-Path $dist1c)) {
 Write-Host "Expand archive 1cv8.zip ..."
 Expand-Archive -Force -Path $dist1c -DestinationPath $path
 Set-Content "$path\bin\conf\conf.cfg" "DisableUnsafeActionProtection=.*;"
-
-$config = "$path\config"
-$database = "$path\database"
-$autotest = "$path\autotest.epf"
-Write-Host "Create infobase"
-New-Item -ItemType Directory -Force -Path "$database" | Out-Null
-Start-Process "bin\1cv8t.exe" -ArgumentList "CREATEINFOBASE File=$database" -Wait
-Write-Host "Load config..."
-Start-Process "bin\1cv8t.exe" -ArgumentList "DESIGNER /F $database /LoadConfigFromFiles $config /UpdateDBCfg"  -Wait
-Write-Host "Process autotest..."
-Start-Process "bin\1cv8ct.exe" -ArgumentList "ENTERPRISE /F $database /C $autotest" -Wait
-Write-Host "Autotest finished"
