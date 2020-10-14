c1c2fead609c343558aa78dd916ce4f5b3e175ba
Kandrashin Denis <mail@lintest.ru>
2020-10-14 19:48:10 +0300

    Замена дистрибутива платформы

--- appveyor.ps1
+++ appveyor.ps1
@@ -1,12 +1,9 @@
 $url1c = $env:URL_1CV8T
 $path = $env:APPVEYOR_BUILD_FOLDER
 
-$dist1c = "$path\1cv8.zip"
+$dist1c = "$path\1cv8t.7z"
 if (!(Test-Path $dist1c)) {
-  Write-Host "Download 1cv8.zip ..."
+  Write-Host "Download 1cv8t.7z ..."
   Invoke-WebRequest -Uri $url1c -OutFile $dist1c
 }
-
-Write-Host "Expand archive 1cv8.zip ..."
-Expand-Archive -Force -Path $dist1c -DestinationPath $path
-Set-Content "$path\bin\conf\conf.cfg" "DisableUnsafeActionProtection=.*;"
+Write-Host "Expand archive 1cv8t.7z ..."
