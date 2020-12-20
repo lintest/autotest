caf0d1ccfdecc667e5f8356c7670ece91b95b02a
Kandrashin Denis <mail@lintest.ru>
2020-12-20 16:16:53 +0300

    Простая конфигурация для тестирования

--- appveyor.ps1
+++
@@ -1,8 +0,0 @@
-$path = $env:APPVEYOR_BUILD_FOLDER
-$url1c = $env:URL_1CV8T
-$dist1c = "$path\1cv8t.7z"
-if (!(Test-Path $dist1c)) {
-  Write-Host "Download 1cv8t.7z ..."
-  Invoke-WebRequest -Uri $url1c -OutFile $dist1c
-}
-Set-Content -Path app_port.txt -Value ([uri] $env:APPVEYOR_API_URL).Port
